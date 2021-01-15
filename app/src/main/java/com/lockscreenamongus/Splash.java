1d32c44c9ead5257d67180151b4d53d87e323d0f
unknown <lonelyheart1402@gmail.com>
2021-01-14 23:13:36 -0800

    hihi

---
+++ app/src/main/java/com/lockscreenamongus/Splash.java
@@ -0,0 +1,75 @@
+package com.lockscreenamongus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+import java.util.Random;
+
+public class Splash extends AppCompatActivity {
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(MainActivity.INTER_ID);
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                Random r = new Random();
+                int ads = r.nextInt(100);
+                if (ads < MainActivity.PERCENT_SHOW_INTER_AD){
+                    if (mInterstitialAd.isLoaded()) {
+                        mInterstitialAd.show();
+                    } else {
+                        Log.d("TAG", "The interstitial wasn't loaded yet.");
+                    }
+                }
+                else {
+                    Intent myIntent = new Intent(Splash.this,MainActivity.class);
+                    Splash.this.startActivity(myIntent);
+                }
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                Intent myIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(myIntent);
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the interstitial ad is closed.
+            }
+        });
+
+
+    }
+
+}
